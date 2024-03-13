d83dc9d29bdd77cc5d4b5ddb12ba611ae6bb4194
NikitaPash <pashchuknik@gmail.com>
2024-03-13 10:47:42 +0200

    added STATIC_ROOT

--- CityVotingPlatform/settings.py
+++ CityVotingPlatform/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/var/www/static/",
 ]
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
