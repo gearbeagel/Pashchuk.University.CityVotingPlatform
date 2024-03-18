79ac981581d3f2753bb6dc52fa856f4550cd0bd4
NikitaPash <pashchuknik@gmail.com>
2024-03-19 01:30:53 +0200

    Updated settings

--- CityVotingPlatform/settings.py
+++ CityVotingPlatform/settings.py
@@ -173,7 +173,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vebvebenko@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_PASSWORD = passwrd
 EMAIL_USE_TLS = True
 
 
